2bd90f720c5d46a996cb596f0fc018c9ad63a608
R Alejandro Mena <alexandro.ramr777@gmail.com>
2025-10-28 19:52:11 -0500

    #1 add vue project and migrate minimal webapp

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        <div id="" class="row">
+            <h1>My medical App</h1>
+        </div>
+        <div id="main_content" class="row">
+            <div class="col-8">
+                <div class="papaya" data-params="params"></div>
+                <a class="mx-2" href="https://rii-mango.github.io/Papaya/"><i>built with Papaya.js</i></a>
+            </div>
+            
+            <div class="col-4" id="right_side_content">
+                <h4>Features:</h4>
+                <ul>
+                <li>
+                    Add image
+                </li>
+                <li>
+                    Close All
+                </li>
+                <li>
+                    Show header info, Show img info
+                </li>
+                <li>
+                    Change color table
+                </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/*
+ This is a custom class to 'stack' elements vertically and centered.
+*/		
+.stacked {
+    display: flex;
+    justify-content: center;
+    align-items: center;		 
+    flex-direction: column;
+}
+
+</style>
--- minimal-web-app/js/main.js
+++ minimal-web-app/js/main.js
@@ -1,7 +1,7 @@
 
-//console.log("Hello");
-//var params = [];
-//params["images"] = ["data/brain-lesion_T1w.nii.gz"];
+console.log("Hello");
+var params = [];
+params["images"] = ["data/brain-lesion_T1w.nii.gz"];
 
 $(document).ready(function () {
     
