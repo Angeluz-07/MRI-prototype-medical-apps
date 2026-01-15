af42ea8f58aa4b001f98a57bfbc873d2c46495d5
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-01-15 16:18:22 -0500

    #4 remove vue files

--- frontend/src/services/FakeService.js
+++
@@ -1,9 +0,0 @@
-export default {
-  process(formData) {
-    // Specific implementation details are hidden here
-    console.log("processing:" , formData)
-    return {"status":"ok"};
-  },
-
-  // ... other user-related methods
-};
