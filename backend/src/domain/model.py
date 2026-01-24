ea632b3fea3c88601df4d5247ef639468cbbf73f
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-01-23 19:51:18 -0500

    #5 add domain models for algorithms and executions

---
+++ backend/src/domain/model.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List
+from uuid import UUID, uuid4
+
+@dataclass
+class Algorithm:
+    id: int 
+    name: str
+    description: str
+
+
+@dataclass
+class Execution:
+    id: int 
+    message: str
+    timestamp: datetime
