ce783b134c083e23b560dde5ffba917506117eb9
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-01-27 16:10:53 -0500

    #5 remove unused references

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from repository import TasksRepository, Task
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -15,31 +14,6 @@ app.add_middleware(
     allow_methods=["*"],
 )
 
-# repository
-repository = TasksRepository()
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/tasks")
-def get_tasks():
-    return {"tasks": repository.get_all()}
-
-
-@app.post("/tasks")
-def add_task(new_task: Task):
-    repository.save(new_task)
-    return {"message": repository.get_all()}
-    
-
-@app.post("/tasks/{id}/")
-def update_task_status(id: int):
-    item = repository.get_by_id(id)
-    item.status = not (item.status)
-    return {"task": item}
 
 from repository import FileRepository
 @app.get("/mri/images")
