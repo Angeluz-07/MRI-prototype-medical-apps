0629f8fbe7095801f24a84a8f1d80fb6be3d2c42
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-02-17 18:53:53 -0500

    #7 add user info in authcontext

--- frontend/src/components/AuthContext.tsx
+++ frontend/src/components/AuthContext.tsx
@@ -5,9 +5,17 @@ import {
   useEffect,
   ReactNode,
 } from "react";
+import axios from "axios"; // Importamos Axios
+
+interface User {
+  username: string;
+  email: string;
+  // add other fields like avatar, bio, etc.
+}
 
 interface AuthContextType {
   isAuthenticated: boolean;
+  user: User | null; // New user state
   login: (access: string, refresh: string) => void;
   logout: () => void;
 }
@@ -15,9 +23,37 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!localStorage.getItem("access_token"),
   );
+
+  // Function to fetch user data using the token
+  const fetchUserData = async () => {
+    try {
+      const API_URL = "http://127.0.0.1:8000/api/me";
+
+      // 1. Get the token from storage
+      const token = localStorage.getItem("access_token");
+
+      const response = await axios.get(API_URL, {
+        // 2. Add the headers object
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(response.data);
+      console.log(response.data)
+    } catch (err) {
+      logout(); // If fetching user fails, token might be invalid
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchUserData();
+    }
+  }, [isAuthenticated]);
 
   const login = (access: string, refresh: string) => {
     localStorage.setItem("access_token", access);
@@ -32,7 +68,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, user }}>
       {children}
     </AuthContext.Provider>
   );
