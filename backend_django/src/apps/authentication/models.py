584e17b50608444c9eba46529cda202d0a1e1482
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-02-18 17:25:23 -0500

    #7 add uuid as id in models

--- backend_django/src/apps/authentication/models.py
+++ backend_django/src/apps/authentication/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     def __str__(self):
         return self.email
--- backend_django/src/apps/mri_analisis/models.py
+++ backend_django/src/apps/mri_analisis/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+import uuid
 
     
 class Algorithm(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=36,
+        default=uuid.uuid4,
+        editable=False
+    )
     name = models.CharField(max_length=200)
     description = models.TextField()
     
