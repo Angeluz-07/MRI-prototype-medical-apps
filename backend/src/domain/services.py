f3b8985e8bd637b41ef3cd47d788633350cdfad4
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-04-21 15:18:09 -0500

    update gitignore

--- backend/src/domain/services.py
+++ backend/src/domain/services.py
@@ -4,13 +4,14 @@ from src.domain.filepath_manager import FilepathManager
 
 import ants
 from antspynet.utilities import brain_extraction
+CACHE_DIR = "/app/data/models"
 
 def _img_process_run(raw_img_path):
     print(f'AntsPy version = {ants.__version__}')
     print("processing...", raw_img_path)
     raw_img_ants = ants.image_read(raw_img_path, reorient='IAL')
     print("img read...")
-    prob_brain_mask = brain_extraction(raw_img_ants,modality="t1",verbose=True) # todo:fix issue of weights being retrieved on running time.
+    prob_brain_mask = brain_extraction(raw_img_ants,modality="t1",verbose=False, antsxnet_cache_directory=CACHE_DIR) # todo:fix issue of weights being retrieved on running time.
     print("prob brain mask extracted...")
     brain_mask = ants.get_mask(prob_brain_mask, low_thresh=0.5)
     print("brain mask got...")
